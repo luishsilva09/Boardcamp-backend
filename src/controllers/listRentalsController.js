17611379131232a5961197a426c21c8d15c73fab
luis <luishsilva09@gmail.com>
2022-07-25 17:23:38 -0300

    ajuste listagem de alugueis uasando busca pelas querys

--- src/controllers/listRentalsController.js
+++ src/controllers/listRentalsController.js
@@ -4,7 +4,7 @@ export async function listRentals(req, res) {
   try {
     const customerId = parseInt(req.query.customerId);
     const gameId = parseInt(req.query.gameId);
-    const status = req.query.status;
+
     const query = `
         SELECT rentals.*,json_build_object('id',customers.id,'name',customers.name) as customer,
         json_build_object('id',games.id,'name',games.name,'categoryId',games."categoryId",'categoryName',categories.name)as game
@@ -12,6 +12,14 @@ export async function listRentals(req, res) {
         JOIN customers ON rentals."customerId"=customers.id 
         JOIN games ON rentals."gameId"=games.id
         JOIN categories ON games."categoryId"=categories.id`;
+
+    if (customerId && gameId) {
+      const { rows: rentalsData } = await connection.query(
+        `${query} WHERE rentals."gameId"=$1 AND rentals."customerId"=$2`,
+        [gameId, customerId]
+      );
+      return res.send(rentalsData);
+    }
     if (customerId) {
       const { rows: rentalsData } = await connection.query(
         `${query} WHERE customers.id=$1`,
